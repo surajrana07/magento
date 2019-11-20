02a2d6f96c9b270f10bbbdd0d5934caae920ab3f
surajrana07 <suraj.rana@orioncoders>
2019-11-20 18:42:26 +0530

    first commit

---
+++ app/design/frontend/Orion/default/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @category  M2
+ * @package   M2
+ * @author    suraj
+ * @copyright Copyright (c) 2015 Redbox Digital (http://www.redboxdigital.com)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/orion/default',
+    __DIR__
+);
